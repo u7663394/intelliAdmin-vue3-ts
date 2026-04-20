7d05911285ae572cbc7a31ac383fa4ae8f921679
u7663394 <u7663394@anu.edu.au>
2026-04-20 23:06:34 +1000

    feat: import and use Element-Plus UI library

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div>我是App</div>
+  <el-button round plain type="primary" :icon="Edit">按钮</el-button>
   <router-view></router-view>
 </template>
 
