3760d17881b4f204ba7fc0da2f1524c96b99734d
u7663394 <u7663394@anu.edu.au>
2026-04-21 14:25:31 +1000

    feat: implement dropdown menu with home, project link, and logout functionality

--- src/layouts/components/NavBar.vue
+++ src/layouts/components/NavBar.vue
@@ -1,17 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user'
+import { removeLocalToken } from '@/utils/auth'
+import { ElMessageBox } from 'element-plus'
+import { useRouter } from 'vue-router'
+
+/**
+ * 下拉菜单
+ *   1. 首页
+ *   2. 项目地址
+ *   3. 退出登录
+ *     3.1. 确认框
+ *     3.2. 清空用户信息
+ *     3.3. 跳转到登录页
+ */
+const router = useRouter()
+const userStore = useUserStore()
+const handleCommand = async (command: string) => {
+  if (command === 'home') {
+    // 1. 首页
+    router.push('/')
+  } else if (command === 'projects') {
+    // 2. 项目地址
+    window.open('https://github.com/u7663394/intelliAdmin-vue3-ts', '_blank')
+  } else if (command === 'logout') {
+    // 3.1. 确认框
+    await ElMessageBox.confirm('您确定要退出登录吗?', '退出提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning',
+    })
+    // 3.2. 清空用户信息
+    userStore.setToken('')
+    removeLocalToken()
+    // 3.3. 跳转到登录页
+    router.push('/login')
+  }
+}
+</script>
 
 <template>
   <div class="nav-bar">
     <!-- breadcrumb -->
     <div></div>
     <!-- dropdown -->
-    <el-dropdown>
+    <el-dropdown @command="handleCommand">
       <div class="user-wrapper">管理员</div>
       <template #dropdown>
         <el-dropdown-menu>
-          <el-dropdown-item>首页</el-dropdown-item>
-          <el-dropdown-item>项目地址</el-dropdown-item>
-          <el-dropdown-item>退出登录</el-dropdown-item>
+          <el-dropdown-item command="home">首页</el-dropdown-item>
+          <el-dropdown-item command="projects">项目地址</el-dropdown-item>
+          <el-dropdown-item command="logout">退出登录</el-dropdown-item>
         </el-dropdown-menu>
       </template>
     </el-dropdown>
